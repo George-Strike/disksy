f2d09113a4a349acfa9c8a0906557002b52daa2c
George Strike <george.strike@outlook.com>
2022-07-13 13:35:02 +0100

    Structs, Trait

--- Source/Backend/Services/DisksyAnalyzerService/src/main.rs
+++ Source/Backend/Services/DisksyAnalyzerService/src/main.rs
@@ -1,18 +1,43 @@
-use std::env;
+use std::{env};
 
 use walkdir::WalkDir;
 
+pub trait SizeConversion {
+    fn as_kb(&self) -> f64;
+    fn as_mb(&self) -> f64;
+}
+struct FileInfo {
+    file_name: String,
+    size: FileSize,
+}
+struct FileSize {
+    size: u64,
+}
+impl SizeConversion for FileSize { 
+    fn as_kb(&self) -> f64 {
+        self.size as f64 / 1024f64
+    }
+    fn as_mb(&self) -> f64 {
+        (self.size as f64 / 1024f64) / 1024f64
+    }
+}
+
 fn main() {
     for entry in WalkDir::new(env::current_dir().unwrap()).follow_links(true) {
-        let ent = entry.unwrap();
-        let size = ent.path().metadata().unwrap().len();
-        let size_as_kb = size as f64 / 1024f64;
-        let size_as_mb = (size as f64 / 1024f64) / 1024f64;
-        if size_as_mb >= 0.1 {
-            println!("path: {}, size {:.3}mb", ent.path().display(), size_as_mb);
+
+        //let ent: &Path = entry.unwrap().path();
+        
+        let file_info: FileInfo = FileInfo {
+            file_name: entry.as_ref().unwrap().path().display().to_string(),
+            size: FileSize {
+                size: entry.unwrap().path().metadata().unwrap().len()
+            }
+        };
+        if file_info.size.as_mb() >= 0.1 {
+            println!("path: {}, size {:.3}mb", file_info.file_name, file_info.size.as_mb());
         } 
         else {
-            println!("path: {}, size {:.3}kb", ent.path().display(), size_as_kb);
+            println!("path: {}, size {:.3}kb", file_info.file_name, file_info.size.as_kb());
         }
     }
 }
