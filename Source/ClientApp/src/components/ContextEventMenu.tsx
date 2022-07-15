4aa235d05a906a38abec4cc0c2d00375b3dfa97f
George Strike <george.strike@outlook.com>
2022-07-15 19:38:47 +0100

    click

--- Source/ClientApp/src/components/ContextEventMenu.tsx
+++ Source/ClientApp/src/components/ContextEventMenu.tsx
@@ -1,3 +1,4 @@
+import { Col, Row } from "antd";
 import { useState, useRef, useEffect, useLayoutEffect } from "react";
 
 export enum MenuEvent {
@@ -14,55 +15,60 @@ interface MenuEventHandler {
     }
 }
 
-const ContextEventMenu: React.FC<MenuEventHandler> = ({...props}) => {
+const ContextEventMenu: React.FC<MenuEventHandler> = ({ ...props }) => {
     const [contextData, setContextData] = useState({ visible: false, posX: 0, posY: 0 });
     const contextRef: any = useRef(null);
 
     useEffect(() => {
         const contextMenuEventHandler = (event: any) => {
-          const targetElement= document.getElementById(props.targetId)
-          if(targetElement && targetElement.contains(event.target)){
-            event.preventDefault();
-            setContextData({ visible: true, posX: event.clientX, posY: event.clientY })
-          }else if(contextRef.current && !contextRef.current.contains(event.target)){
-            setContextData({ ...contextData, visible: false })
-          }
+            const targetElement = document.getElementById(props.targetId)
+            if (targetElement && targetElement.contains(event.target)) {
+                event.preventDefault();
+                setContextData({ visible: true, posX: event.clientX, posY: event.clientY })
+            } else if (contextRef.current && !contextRef.current.contains(event.target)) {
+                setContextData({ ...contextData, visible: false })
+            }
         }
-    
+
         const offClickHandler = (event: any) => {
-          if(contextRef.current && !contextRef.current.contains(event.target)){
-            setContextData({ ...contextData, visible: false })
-          }
+            if (contextRef.current && !contextRef.current.contains(event.target)) {
+                setContextData({ ...contextData, visible: false })
+            }
         }
-    
+
         document.addEventListener('contextmenu', contextMenuEventHandler)
         document.addEventListener('click', offClickHandler)
         return () => {
-          document.removeEventListener('contextmenu', contextMenuEventHandler)
-          document.removeEventListener('click', offClickHandler)
+            document.removeEventListener('contextmenu', contextMenuEventHandler)
+            document.removeEventListener('click', offClickHandler)
         }
-      }, [contextData, props.targetId])
-    
-      useLayoutEffect(() => {
-        if(contextData.posX + contextRef.current?.offsetWidth > window.innerWidth){
-          setContextData({ ...contextData, posX: contextData.posX - contextRef.current?.offsetWidth})
+    }, [contextData, props.targetId])
+
+    useLayoutEffect(() => {
+        if (contextData.posX + contextRef.current?.offsetWidth > window.innerWidth) {
+            setContextData({ ...contextData, posX: contextData.posX - contextRef.current?.offsetWidth })
         }
-        if(contextData.posY + contextRef.current?.offsetHeight > window.innerHeight){
-          setContextData({ ...contextData, posY: contextData.posY - contextRef.current?.offsetHeight})
+        if (contextData.posY + contextRef.current?.offsetHeight > window.innerHeight) {
+            setContextData({ ...contextData, posY: contextData.posY - contextRef.current?.offsetHeight })
         }
-      }, [contextData])
-    
-      return (
-        <div ref={contextRef} className='contextMenu' style={{ display:`${contextData.visible ? 'block' : 'none'}`, left: contextData.posX, top: contextData.posY }}>
-          <div className={`optionsList ${props.classes?.listWrapper}`}>
-            {props.options.map((option) => (
-              <li key={option} className={`optionListItem ${props.classes?.listItem}`}>
-                {option}
-              </li>
-            ))}
-          </div>
-        </div>
-      );
+    }, [contextData])
+
+    return (
+        <Row>
+            <Col>
+                <div ref={contextRef} className='contextMenu' style={{ display: `${contextData.visible ? 'block' : 'none'}`, left: contextData.posX, top: contextData.posY }}>
+                    <div className={`optionsList ${props.classes?.listWrapper}`}>
+                        {props.options.map((option) => (
+                            <li key={option} className={`optionListItem ${props.classes?.listItem}`}>
+                                {option}
+                            </li>
+                        ))}
+                    </div>
+                </div>
+            </Col>
+        </Row>
+
+    );
 }
 
 export default ContextEventMenu;
--- Source/ClientApp/src/components/Directory.tsx
+++ Source/ClientApp/src/components/Directory.tsx
@@ -1,27 +1,39 @@
 import { useEffect, useState } from 'react'
-import { Col, Row, Table } from 'antd';
+import { Col, Dropdown, Menu, Row, Table } from 'antd';
 import React from 'react';
 import { DirectoryInfo } from '../bindings/directory';
 import Column from 'antd/lib/table/Column';
 import { TableRowSelection } from 'antd/lib/table/interface';
-import ContextEventMenu, { MenuEvent } from './ContextEventMenu';
-import './Directory.css'
+
 
 const Directory: React.FC<{ directoryPath: string }> = ({ directoryPath }) => {
     const [directoryValues, setDirectoryValues] = useState<DirectoryInfo[]>([]);
 
+    const Item = Menu.Item
+    const menu = (value: any) =>
+        <Menu>
+            <Item>{value}</Item>
+            <Item>Like it</Item>
+            <Item>Bookmark</Item>
+        </Menu>
+
+    const render = (value: any) =>
+        <Dropdown overlay={menu(value)} trigger={[`contextMenu`]}>
+            <div>{value}</div>
+        </Dropdown>
+
     const rowSelection: TableRowSelection<DirectoryInfo> = {
         onChange: (selectedRowKeys: any, selectedRows: any) => {
-          console.log(`selectedRowKeys: ${selectedRowKeys}`, 'selectedRows: ', selectedRows);
+            console.log(`selectedRowKeys: ${selectedRowKeys}`, 'selectedRows: ', selectedRows);
         },
         onSelect: (record: any, selected: any, selectedRows: any) => {
-          console.log(record, selected, selectedRows);
+            console.log(record, selected, selectedRows);
         },
         onSelectAll: (selected: any, selectedRows: any, changeRows: any) => {
-          console.log(selected, selectedRows, changeRows);
+            console.log(selected, selectedRows, changeRows);
         },
-      };
-    
+    };
+
 
     const getData = () => {
         fetch(`http://localhost:9876/directory/${directoryPath}`)
@@ -44,24 +56,16 @@ const Directory: React.FC<{ directoryPath: string }> = ({ directoryPath }) => {
     }, []);
 
 
-    return <div style={{ paddingTop: "2em" }} id="directoryDataTable" className="directoryDataTable">
-         <ContextEventMenu
-          targetId='directoryDataTable'
-          options={[MenuEvent.Delete, MenuEvent.Update]}
-          classes={{
-            listWrapper: 'directoryDataTableListWrapper',
-            listItem: 'directoryDataTableListItem'
-          }}
-        />
+    return <div style={{ paddingTop: "2em" }}>
         <Row>
             <Col span={20}>
-                <Table 
+                <Table
                     childrenColumnName='files'
                     dataSource={directoryValues}
-                    rowSelection={{ ...rowSelection}}
+                    rowSelection={{ ...rowSelection }}
                 >
-                    <Column title="Directory" dataIndex="name" key="name" />
-                    <Column title="Size" dataIndex="size" key="size" />                    
+                    <Column title="Directory" dataIndex="name" key="name" render={render} />
+                    <Column title="Size" dataIndex="size" key="size" render={render} />
                 </Table>
             </Col>
         </Row>
