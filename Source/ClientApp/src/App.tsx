e9de61809b06c72109f79c1f1e4e1e64f96d4ab6
George Strike <george.strike@outlook.com>
2022-07-15 09:17:58 +0100

    changes

--- Source/ClientApp/src/App.tsx
+++ Source/ClientApp/src/App.tsx
@@ -33,20 +33,26 @@ const App: React.FC = () => {
   }, []);
 
   return <Row>
-    {diskValues.map(x => <Col span={6}>
+    {diskValues.map(x =>{ 
+      const usedPercentage: number = calculateTotalUsedPercentage(x.disk_size_info.total_space_gb, x.disk_size_info.available_space_gb);
+      return <Col span={6}>
       <Row>
-        <Col span={12}><p>Disk Label: {x.label}<br></br> Name: {x.name !== "" ? x.name : "Local Drive"}<br></br>
+        <Col span={12}>
+          <h3>Disk Info</h3>
+          <p>Disk Label: {x.label}<br></br> Name: {x.name !== "" ? x.name : "Local Drive"}<br></br>
           Remaining Space: {x.disk_size_info.available_space_gb}gb/{x.disk_size_info.total_space_gb}gb <br></br> Disk Type: {x.disk_type}<br></br></p>
         </Col>
 
         <Col span={12}>
+          <h3>Percent Used</h3>
         <Progress type="circle"
-          strokeColor={calculateTotalUsedPercentage(x.disk_size_info.total_space_gb, x.disk_size_info.available_space_gb) > 75 ? "red" : calculateTotalUsedPercentage(x.disk_size_info.total_space_gb, x.disk_size_info.available_space_gb) >= 50 ? "#d4b402" : "green"}
-          percent={calculateTotalUsedPercentage(x.disk_size_info.total_space_gb, x.disk_size_info.available_space_gb)} />
+          strokeColor={usedPercentage > 75 ? "red" : usedPercentage >= 50 ? "#d4b402" : "green"}
+          percent={usedPercentage} />
       
         </Col>
       </Row>
-    </Col>)}
+    </Col>
+  })}
   </Row>
 
 }
